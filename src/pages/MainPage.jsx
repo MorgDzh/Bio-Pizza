0cfa158d1870e0d2fc7cf9a561772cbbdc6844a9
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-04-28 20:50:45 +0600

    added pagination

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import { Container } from "@mui/material";
 import React, { useContext } from "react";
 import { useEffect } from "react";
 import FiltersBlock from "../components/FiltersBlock";
+import MyPagination from "../components/MyPagination";
 import ProductCard from "../components/ProductCard";
 import { clientContext } from "../contexts/ClientContext";
 
@@ -24,6 +25,7 @@ const MainPage = () => {
             <ProductCard key={item.id} item={item} />
           ))}
         </div>
+        <MyPagination />
       </Container>
     </div>
   );
